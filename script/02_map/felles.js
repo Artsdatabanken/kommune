e3beade7a5d7db2f761807432a3e9d7f60aba9d0
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-20 02:49:55 +0200

    Gjør mapping generisk, bruk navn fra sparql

--- script/02_map/felles.js
+++ script/02_map/felles.js
@@ -3,7 +3,7 @@ const { io, log } = require("lastejobb");
 const lesSparqlOutput = fil => io.lesDatafil(fil).results.bindings;
 
 function konverter(nivå) {
-  const r = lesElementer(nivå);
+  const r = lesElementer(nivå, "item");
   flettNaboer(r, nivå);
   flettMedBilder(r, nivå);
   const medNummerSomNøkkel = mapTilNummerSomNøkkel(r);
@@ -21,34 +21,30 @@ function mapTilNummerSomNøkkel(r) {
   }, {});
 }
 
-function lesElementer(filnavn) {
+function lesElementer(filnavn, nøkkelfelt) {
   const elementer = lesSparqlOutput(filnavn);
   const r = {};
   elementer.forEach(e => {
-    const k = {
-      wikidata: e.item.value,
-      bilde: { image: [], banner: [] }
-    };
-    add(k, "label", e.itemLabel);
-    add(k, "inception", e.inception);
-    add(k, "dissolved", e.dissolved);
-    add(k, "code", e.code);
-    add(k, "wikipedia", e.article);
-    add(k, "elevation", e.elevation);
-    add(k, "url", e.url);
-    add(k.bilde, "coa", e.coa);
-
-    r[k.wikidata] = k;
+    const k = map(e);
+    const nøkkel = k[nøkkelfelt];
+    r[nøkkel] = k;
   });
   return r;
 }
 
-function add(o, key, field) {
-  if (!field) return;
-  let value = field.value;
-  if (field.datatype === "http://www.w3.org/2001/XMLSchema#dateTime")
-    value = new Date(value);
-  if (value) o[key] = value;
+function map(e) {
+  const r = {};
+  Object.entries(e).forEach(([key, v]) => {
+    r[key] = value(v);
+  });
+  return r;
+}
+
+function value(e) {
+  if (!e) return null;
+  if (e.datatype === "http://www.w3.org/2001/XMLSchema#dateTime")
+    return new Date(e.value);
+  return e.value;
 }
 
 function flettNaboer(r, nivå) {
@@ -68,17 +64,13 @@ function flettMedBilder(r, nivå) {
   bilde.forEach(e => {
     const id = e.item.value;
     const fra = r[id];
+    fra.bilde = fra.bilde || { image: [], banner: [] };
     const bilder = fra.bilde;
     const image = value(e.image);
     if (image) bilder.image.push(image);
     const banner = value(e.banner);
     if (banner) bilder.banner.push(banner);
   });
-}
-
-function value(e) {
-  if (!e) return null;
-  return e.value;
 }
 
 module.exports = { konverter };
