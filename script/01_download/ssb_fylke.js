7ca4672b29828c7ed59f1426414b63ca45c24002
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-03 14:23:50 +0200

    Flagg SSB som ikke i bruk

--- script/01_download/ssb_fylke.js
+++ script/01_download/ssb_fylke.js
@@ -1,26 +1,12 @@
-const path = require("path");
 const { io, http, log } = require("lastejobb");
 
-// Leser fylkers navn fra SSB
-function mapFylker(kilde) {
-  let r = {};
-  kilde.classificationItems.forEach(ci => {
-    const origName = ci.name;
-    if (ci.code !== "99") {
-      r[ci.code] = {
-        tittel: { nb: ci.name }
-      };
-    }
-  });
-  return r;
-}
-
+// Ikke i bruk
 async function importFylker() {
   let fylker = await http.downloadJson(
     "https://data.ssb.no/api/klass/v1/versions/916.json?language=nb",
     "ssb_fylke_rå.json"
   );
-  return mapFylker(fylker);
+  return fylker;
 }
 
 importFylker()
--- script/01_download/ssb_kommune.js
+++ script/01_download/ssb_kommune.js
@@ -1,62 +1,7 @@
-const path = require("path");
-const { io, log, http } = require("lastejobb");
+const { io, http, log } = require("lastejobb");
 
 // Leser kommuners navn fra SSB
-// TODO: Vurder å lese fra Kartverket:
-// https://ws.geonorge.no/kommuneinfo/v1/#/default/get_fylkerkommuner
-// Fullstendig mangel på historikk :(
-
-function parseSpråk(s) {
-  switch (s) {
-    case "Gáivuotna - Kåfjord - Kaivuono":
-      return "Kåfjord";
-    case "Fauske - Fuosko":
-      return "Fauske";
-    case "Hamarøy - Hábmer":
-      return "Hamarøy";
-    case "Divtasvuodna - Tysfjord":
-      return "Tysfjord";
-    case "Sortland - Suortá":
-      return "Sortland";
-    case "Harstad - Hárstták":
-      return "Harstad";
-    case "Loabák - Lavangen":
-      return "Lavangen";
-    case "Storfjord - Omasvuotna - Omasvuono":
-      return "Storfjord";
-    case "Gáivuotna - Kåfjord - Kaivuono":
-      return "Kåfjord";
-    case "Guovdageaidnu - Kautokeino":
-      return "Kautokeino";
-    case "Porsanger - Porsángu - Porsanki":
-      return "Porsanger";
-    case "Kárášjohka - Karasjok":
-      return "Karasjok";
-    case "Deatnu - Tana":
-      return "Tana";
-    case "Unjárga - Nesseby":
-      return "Nesseby";
-    case "Snåase - Snåsa":
-      return "Snåsa";
-    case "Raarvihke - Røyrvik":
-      return "Røyrvik";
-  }
-  return s;
-}
-
-function mapKommuner(kommuner) {
-  r = {};
-  kommuner.forEach(ci => {
-    // AO_18-50
-    const kode = "AO-" + ci.code.substring(0, 2) + "-" + ci.code.substring(2);
-    if (ci.code !== "9999") {
-      r[kode] = {
-        tittel: { nb: parseSpråk(ci.name) }
-      };
-    }
-  });
-  return r;
-}
+// Ikke i bruk
 
 async function importKommuner() {
   var dato = new Date();
@@ -66,7 +11,7 @@ async function importKommuner() {
 
   const url = `http://data.ssb.no/api/klass/v1/classifications/131/codes.json?from=${from}&to=${to}`;
   let kommuner = await http.downloadJson(url, "inn_ssb.json");
-  return mapKommuner(kommuner.codes);
+  return kommuner.codes;
 }
 
 importKommuner()
