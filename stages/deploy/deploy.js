e4274fde5c68b353f8fa6fcead3ea8438594711d
Stig Thorseth <stig.thorseth@artsdatabanken.no>
2023-02-06 16:14:06 +0100

    updated deploy

--- stages/deploy/deploy.js
+++ stages/deploy/deploy.js
@@ -11,68 +11,80 @@ let kommune_repo_url = "https://github.com/Artsdatabanken/kommune.git";
  * Run an arbitrary git command.
  */
 function git(cmd, args = "") {
-  var out = execSync("git " + cmd + " " + args, { skipThrow: true });
-  //new TextDecoder().decode(out);
-  var out_string = new TextDecoder('utf-8').decode(out);
-  return out_string;
-  //console.log(out);
+    var out = execSync("git " + cmd + " " + args, { skipThrow: true });
+    //new TextDecoder().decode(out);
+    var out_string = new TextDecoder('utf-8').decode(out);
+    return out_string;
+    //console.log(out);
 }
 
-console.log("Running deploy for kommune-lastejobb!!"); 
+console.log("Running deploy for kommune-lastejobb!!");
 
 // check if upstream exists
-try{
-    let remotes = git("remote -v");
-    console.log(remotes);
-    if(remotes.includes(kommune_repo_url)){
-        console.log("(repo for kommune finnes, trengs ikke å legges til..)");
-        kommune_repo_is_added = true;
-    }else{
-         console.log("repo finnes ikke");       
-    }
-}catch(e){
+function check_if_upstream_exists() {
+    try {
+        let remotes = git("remote -v");
+        console.log(remotes);
+        if (remotes.includes(kommune_repo_url)) {
+            console.log("(repo for kommune finnes, trengs ikke å legges til..)");
+            kommune_repo_is_added = true;
+        } else {
+            console.log("repo finnes ikke");
+        }
+    } catch (e) {
         // i dont care....
-        console.log("Error: "+e);
+        console.log("Error: " + e);
+    }
 }
 
 // Innkluderer Artsdatabanken/kommune som repo under alias upstream hvis den ikke allerede er lagt til 
-if(!kommune_repo_is_added){
-    try{    
-        git("remote add upstream https://github.com/Artsdatabanken/kommune.git");
-    }catch(e){
+function add_kommmunerepo_if_not_added() {
+    if (!kommune_repo_is_added) {
+        try {
+            git("remote add upstream https://github.com/Artsdatabanken/kommune.git");
+        } catch (e) {
             // i dont care....
-            console.log("Error: "+e);
+            console.log("Error: " + e);
+        }
     }
 }
 
-try{
-    git("add build/fylke.json");
-    git("add build/fylke.schema.json");
-    git("add build/kommune.json");
-    git("add build/kommune.schema.json");
-    git("add build/dummy.txt");
-}catch(e){
-    // still dont care..
-    console.error("error during git add section: "+e);
+function add_new_files_to_be_commited() {
+    try {
+        git("add build/fylke.json");
+        git("add build/fylke.schema.json");
+        git("add build/kommune.json");
+        git("add build/kommune.schema.json");
+        git("add build/dummy.txt");
+    } catch (e) {
+        // still dont care..
+        console.error("error during git add section: " + e);
+    }
 }
 
-var stamp = Date.now();
 
-try{    
-    let msg  = git('commit -m "deploy '+stamp+'"');
-    console.log(msg);
-}catch(e){
-    //...jadajada..
-    console.log("Error: "+e);
+function make_new_commit() {
+    var stamp = Date.now();
+
+    try {
+        let msg = git('commit -m "deploy ' + stamp + '"');
+        console.log(msg);
+    } catch (e) {
+        //...jadajada..
+        console.log("Error: " + e);
+    }
 }
 
-try{
-    let msg  = git("push --force -u upstream master");  //git push -u gh_nd_brreg main
-    console.log(msg);
-}catch(e){
-    //..move on..
-    console.log("Error: "+e);
+function deploy_with_push() {
+    try {
+        let msg = git("push --force -u upstream master");  //git push -u gh_nd_brreg main
+        console.log(msg);
+    } catch (e) {
+        //..move on..
+        console.log("Error: " + e);
+    }
 }
+
 //git("add build/*");
 // Sjekke om kommunerepo allerede ligger inne med: '
 
