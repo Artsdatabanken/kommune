9e3d399d27a6ba004167e4263d759e5b33a6baaa
Stig Thorseth <stig.thorseth@artsdatabanken.no>
2023-02-15 11:45:55 +0100

    deploy.js

--- stages/deploy/deploy.js
+++ stages/deploy/deploy.js
@@ -2,15 +2,15 @@ const path = require("path");
 const fs = require("fs");
 var textEncoding = require('text-encoding');
 const execSync = require("child_process").execSync;
-let kommune_repo_is_added = false;
-let kommune_repo_url = "https://github.com/Artsdatabanken/kommune.git";
+let deploy_repo_is_added = false;
+let deploy_repo_url = "https://github.com/Artsdatabanken/kommune.git";
 //var gitconfig = require("./gitconfig/gitconfiglocalSync");
 
 
 /**
  * Run an arbitrary git command.
  */
-function git(cmd, args = "") {
+function git(cmd, args = "") {// : string
     var out = execSync("git " + cmd + " " + args, { skipThrow: true });
     //new TextDecoder().decode(out);
     var out_string = new TextDecoder('utf-8').decode(out);
@@ -18,16 +18,23 @@ function git(cmd, args = "") {
     //console.log(out);
 }
 
-console.log("Running deploy for kommune-lastejobb!!");
+function changes_to_deploy_files_found(){// : boolean
+    var change = false;
+    var status = git("ls-files --deleted --modified --others --exclude-standard -- :/");
+    if(status.includes("fylke.") || status.includes("kommune.") || status.includes("dummy.")){
+        change = true;
+    }
+    return change;
+}
 
 // check if upstream exists
-function check_if_upstream_exists() {
+function check_if_upstream_exists() {// void, setting bool-var
     try {
         let remotes = git("remote -v");
-        console.log(remotes);
-        if (remotes.includes(kommune_repo_url)) {
-            console.log("(repo for kommune finnes, trengs ikke å legges til..)");
-            kommune_repo_is_added = true;
+        //console.log(remotes);
+        if (remotes.includes(deploy_repo_url)) {
+            console.log("('deployrepo' allready exist in the local repo list, continuing process.. )");
+            deploy_repo_is_added = true;
         } else {
             console.log("repo finnes ikke");
         }
@@ -38,10 +45,11 @@ function check_if_upstream_exists() {
 }
 
 // Innkluderer Artsdatabanken/kommune som repo under alias upstream hvis den ikke allerede er lagt til 
-function add_kommmunerepo_if_not_added() {
-    if (!kommune_repo_is_added) {
+function add_deployrepo_if_not_added() {// : void
+    if (!deploy_repo_is_added) {
         try {
-            git("remote add upstream https://github.com/Artsdatabanken/kommune.git");
+            console.log("Legger til repo for deploy av kartfiler (*.json)");
+            git("remote add deployrepo https://github.com/Artsdatabanken/kommune.git");
         } catch (e) {
             // i dont care....
             console.log("Error: " + e);
@@ -49,7 +57,7 @@ function add_kommmunerepo_if_not_added() {
     }
 }
 
-function add_new_files_to_be_commited() {
+function add_new_files_to_be_commited() {// : void
     try {
         git("add build/fylke.json");
         git("add build/fylke.schema.json");
@@ -77,7 +85,7 @@ function make_new_commit() {
 
 function getShaOfLastCommit() { //-> string: msg
     try {
-        let msg = git('git rev-parse HEAD');
+        let msg = git('rev-parse HEAD');
         //console.log(msg);
         return msg;
     } catch (e) {
@@ -90,7 +98,7 @@ function deploy_with_push() { // void
     try {
         //let msg = git("push --force -u upstream master");  //git push -u gh_nd_brreg main
         let lastsha = getShaOfLastCommit();
-        let cmd = "push upstream "+lastsha+":master";
+        let cmd = "push deployrepo "+lastsha+":master";
         let msg = git(cmd);
         //git push <remotename> <commit SHA>:<remotebranchname>
         console.log(msg);
@@ -101,9 +109,15 @@ function deploy_with_push() { // void
 }
 
 //The steps
+console.log("**********  Running deploy for kommune-lastejobb  **********\n");
 check_if_upstream_exists();
-add_kommmunerepo_if_not_added();
-add_new_files_to_be_commited();
-make_new_commit();
-console.log(getShaOfLastCommit());
+add_deployrepo_if_not_added();
+if(changes_to_deploy_files_found()){
+    add_new_files_to_be_commited();
+    make_new_commit();
+    console.log("Deploying to deployrepo: ", getShaOfLastCommit());
 //deploy_with_push();
+}else{
+    console.log("!! Found no changed deployfile, aborting further git tasks (deployfiles are: 'fylke.*' or 'kommune.*' or 'dummy.*' )");
+}
+console.log("\n\n**********  Done  **********\n\n\n");
