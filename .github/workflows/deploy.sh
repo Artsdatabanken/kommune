6a6db40e09c8af8328b625a9ce2b1a4bf6fc7bad
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2023-08-28 00:05:09 +0200

    feat: Build using Github Actions

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,5 @@
+cp build/fylke.json destinationRepo/
+cp build/fylke.schema.json destinationRepo/
+cp build/kommune.json destinationRepo/
+cp build/kommune.schema.json destinationRepo/
+tar cf artifacts.tar build
