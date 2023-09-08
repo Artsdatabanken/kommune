e9e0bc3e97b3d02e65479174cff2cd2d302b5ac6
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2023-09-08 11:30:50 +0200

    Tweak release file contents
    - Enable gzip compression (tar.gz)
    - Avoid extra directory 'build' being included in .tar file

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,4 +2,4 @@ cp build/fylke.json destinationRepo/
 cp build/fylke.schema.json destinationRepo/
 cp build/kommune.json destinationRepo/
 cp build/kommune.schema.json destinationRepo/
-tar cf artifacts.tar build
+tar czf artifacts.tar -C build .
