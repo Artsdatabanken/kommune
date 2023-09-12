af3820cedde98a24c7b363bcab6a4b5ae5ec0db9
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2023-09-12 15:06:06 +0200

    Bruk repo navn på releasen for å få unike navn ved nedlasting

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,4 +2,4 @@ cp build/fylke.json destinationRepo/
 cp build/fylke.schema.json destinationRepo/
 cp build/kommune.json destinationRepo/
 cp build/kommune.schema.json destinationRepo/
-tar czf artifacts.tar -C build .
+tar czf $(basename $GITHUB_REPOSITORY).tar.gz -C build .
